97fc73ccca5a187b620542c6daee4da16ee22db3
root <root@DESKTOP-V4EO9N9.>
2025-03-18 19:22:33 +0000

    fixed norminette error

--- src/builtin/unset.c
+++ src/builtin/unset.c
@@ -6,7 +6,7 @@
 /*   By: mmiguelo <mmiguelo@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 11:21:06 by mmiguelo          #+#    #+#             */
-/*   Updated: 2025/03/17 18:00:07 by mmiguelo         ###   ########.fr       */
+/*   Updated: 2025/03/18 19:22:20 by mmiguelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	ft_erase_var(char *var, t_shell *shell)
 {
-	int i;
+	int	i;
 
 	i = get_env_line(var, shell);
 	if (i >= 0)
@@ -31,17 +31,17 @@ void	ft_erase_var(char *var, t_shell *shell)
 
 int	ft_unset(char **args, t_shell *shell)
 {
-	int i;
-	char *error;
+	int		i;
+	char	*error;
 
 	i = 0;
 	(void)shell;
 	if (args[1] && args[1][0] == '-')
 	{
 		error = ft_strdup_free(ft_strjoin("unset: ", ft_strdup(args[1])),
-			 ft_strdup(":invalid option\n"));
+				ft_strdup(":invalid option\n"));
 		if (!error)
-			return(errno);
+			return (errno);
 		print_msg_error(error);
 		free(error);
 	}
