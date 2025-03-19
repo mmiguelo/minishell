be797068b39ade3dc93d7ed582e2d5f54bcefc5b
root <root@DESKTOP-V4EO9N9.>
2025-03-19 00:43:07 +0000

    added comment for next steps

--- src/builtin/export.c
+++ src/builtin/export.c
@@ -6,21 +6,25 @@
 /*   By: mmiguelo <mmiguelo@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 11:20:37 by mmiguelo          #+#    #+#             */
-/*   Updated: 2025/03/17 14:24:00 by mmiguelo         ###   ########.fr       */
+/*   Updated: 2025/03/19 00:31:29 by mmiguelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-/* int	ft_export(char **args, t_shell *shell)
-{
-	if (!shell)
-	return (0);
-} */
+
+/* Checking for Arguments
+If args[1] == NULL, list all exported variables.
+
+Adding or Updating an Environment Variable
+1. Parse VAR=VALUE format.
+2. Validate the variable name.
+3. Check if it already exists in envp.
+4. If it exists, update it.
+5. If not, add it to the list. */
 
 int	ft_export(char **args, t_shell *shell)
 {
-	(void)args;
-	(void)shell;
+	int		i;
 	return (0);
 }
