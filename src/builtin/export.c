ececbcdb9ed2449ce25cefa654b79c9c92ecf817
root <root@DESKTOP-V4EO9N9.>
2025-03-19 10:37:06 +0000

    added more functions to file

--- src/builtin/export.c
+++ src/builtin/export.c
@@ -6,7 +6,7 @@
 /*   By: mmiguelo <mmiguelo@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 11:20:37 by mmiguelo          #+#    #+#             */
-/*   Updated: 2025/03/19 00:31:29 by mmiguelo         ###   ########.fr       */
+/*   Updated: 2025/03/19 10:24:42 by mmiguelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,60 @@
 If args[1] == NULL, list all exported variables.
 
 Adding or Updating an Environment Variable
-1. Parse VAR=VALUE format.
-2. Validate the variable name.
+1. Parse VAR=VALUE format. check
+2. Validate the variable name. check
 3. Check if it already exists in envp.
 4. If it exists, update it.
 5. If not, add it to the list. */
 
+int	ft_check_value(char *arg)
+{
+	int i;
+	
+	i = 0;
+	while (arg[i] != '\0')
+	{
+		if (arg[i] == '=')
+			return (i);
+		i++;
+	}
+	return (0);
+}
+
+int	validate_var(char *var)
+{
+	int	i;
+
+	i = 0;
+	if (!var || (var[i] != '_' && !ft_isalpha(var[i])))
+		return (0);
+	while (++i)
+	{
+		if (var[i] != '_' && !ft_isalnum(var[i]))
+			return (0);
+	}
+	return (i);
+}
+
+int	check_if_var_is_in_env(char *arg)
+{
+	
+}
+
+void	ft_print_export(t_shell *shell)
+{
+	
+}
+
 int	ft_export(char **args, t_shell *shell)
 {
-	int		i;
-	return (0);
+	if (ft_strncmp(args[0], "export", 7) == 0)
+		ft_print_export(shell);
+	else
+	{
+		if (ft_check_value(args[1]) == 0)
+			ft_add_var(args[1], shell);
+		else
+			
+	}
 }
