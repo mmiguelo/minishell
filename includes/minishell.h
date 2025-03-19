5e370f6100e8a35d491ccab5d006ab6b936b6889
root <root@DESKTOP-V4EO9N9.>
2025-03-19 10:35:31 +0000

    add avr to struct

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: mmiguelo <mmiguelo@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 11:11:42 by mmiguelo          #+#    #+#             */
-/*   Updated: 2025/03/18 19:05:34 by mmiguelo         ###   ########.fr       */
+/*   Updated: 2025/03/19 09:13:45 by mmiguelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@ typedef struct s_shell
 {
 	char		***envp;
 	char		**cmd;
+	char		
 }				t_shell;
 
 //function pointer type for builtins
