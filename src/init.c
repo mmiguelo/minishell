2dd83e6d00146c3ce652aabc8e09181c6b4b0ddb
mmiguelo <marco_miguelote@hotmail.com>
2025-03-20 10:48:37 +0000

    cleared norminette errors

--- src/init.c
+++ src/init.c
@@ -6,13 +6,13 @@
 /*   By: mmiguelo <mmiguelo@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 11:26:03 by mmiguelo          #+#    #+#             */
-/*   Updated: 2025/03/20 10:42:47 by mmiguelo         ###   ########.fr       */
+/*   Updated: 2025/03/20 10:47:20 by mmiguelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-char 	**init_env(char **envp)
+char	**init_env(char **envp)
 {
 	int		i;
 	char	**new_env;
