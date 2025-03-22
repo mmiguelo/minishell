ed341f36c9430e20e94280c2b00abc0a122c72f6
root <root@DESKTOP-V4EO9N9.>
2025-03-22 23:53:56 +0000

    added function ft_isupper to libft

---
+++ libft/ft_isupper.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isupper.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmiguelo <mmiguelo@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/22 23:52:43 by mmiguelo          #+#    #+#             */
+/*   Updated: 2025/03/22 23:52:43 by mmiguelo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/**
+ * @brief Check if a string only contains uppercase letters
+ * 
+ * @param str The string to be checked
+ * @return int 1 if the character is an uppercase letter, 0 otherwise
+ */
+int	ft_isupper(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] < 'A' || str[i] > 'Z')
+			return (0);
+		i++;
+	}
+	return (1);
+}
