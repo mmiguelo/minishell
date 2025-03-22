6561eb204d38013da04f1556769a7ce5eaec8d24
root <root@DESKTOP-V4EO9N9.>
2025-03-22 23:55:30 +0000

    added ft_islower to .h

--- libft/libft.h
+++ libft/libft.h
@@ -93,6 +93,7 @@ int		ft_isalnum(int c);
 int		ft_isalpha(int c);
 int		ft_isascii(int c);
 int		ft_isdigit(int c);
+int		ft_islower(char *str);
 int		ft_isnum(char *str);
 int		ft_isprint(int c);
 
