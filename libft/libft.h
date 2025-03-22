984a348b7729f17aaa94599e3dfcb82a80053640
root <root@DESKTOP-V4EO9N9.>
2025-03-22 23:54:48 +0000

    changed order of functions

--- libft/libft.h
+++ libft/libft.h
@@ -89,11 +89,11 @@ void	ft_putstr_fd(char *s, int fd);
 #                               CHECKERS                                       #
 #=============================================================================*/
 
-int		ft_isnum(char *str);
 int		ft_isalnum(int c);
 int		ft_isalpha(int c);
 int		ft_isascii(int c);
 int		ft_isdigit(int c);
+int		ft_isnum(char *str);
 int		ft_isprint(int c);
 
 /*=============================================================================#
