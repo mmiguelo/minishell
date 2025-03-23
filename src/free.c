56b239473289ce4c517b4ecf1f940bfced2faa0b
root <root@DESKTOP-V4EO9N9.>
2025-03-23 00:54:13 +0000

    cleared norm errors

--- src/free.c
+++ src/free.c
@@ -6,13 +6,13 @@
 /*   By: mmiguelo <mmiguelo@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 14:37:52 by mmiguelo          #+#    #+#             */
-/*   Updated: 2025/03/20 12:13:26 by mmiguelo         ###   ########.fr       */
+/*   Updated: 2025/03/23 00:52:54 by mmiguelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	free_envp(t_bt *shell, int i)
+int	free_envp(t_bt *shell, int i)
 {
 	while (i > 0)
 	{
@@ -21,4 +21,18 @@ void	free_envp(t_bt *shell, int i)
 	free(shell->envp[0]);
 	free(shell->envp);
 	shell->envp = NULL;
+	return (0);
+}
+
+int	free_cmd(t_bt *shell, int i)
+{
+	while (i > 0)
+	{
+		free(shell->cmd[--i]);
+		i++;
+	}
+	free(shell->cmd[0]);
+	free(shell->cmd);
+	shell->cmd = NULL;
+	return (0);
 }
