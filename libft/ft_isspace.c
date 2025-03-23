33db8236f7e4155ee4eb13ccd4acf4882ac04106
root <root@DESKTOP-V4EO9N9.>
2025-03-23 00:00:52 +0000

    added ft_isspace.c to libft

---
+++ libft/ft_isspace.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isspace.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmiguelo <mmiguelo@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/22 23:58:34 by mmiguelo          #+#    #+#             */
+/*   Updated: 2025/03/22 23:58:34 by mmiguelo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/**
+ * @brief Check if a character is a whitespace character
+ * 
+ * @param c The character to be checked
+ * @return int 1 if the character is a whitespace character, 0 otherwise
+ */
+int	ft_isspace(int c)
+{
+	if (c == ' ' || c == '\t' || c == '\n' || c == '\v' || c == '\f' || c == '\r')
+		return (1);
+	return (0);
+}
