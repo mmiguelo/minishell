dd6e9912e22ee67ca96ff483c629b71cf7ad4332
mmiguelo <marco_miguelote@hotmail.com>
2025-03-26 13:07:19 +0000

    added documentation to functions

--- src/builtin/builtin_utils.c
+++ src/builtin/builtin_utils.c
@@ -6,12 +6,24 @@
 /*   By: mmiguelo <mmiguelo@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 11:18:53 by mmiguelo          #+#    #+#             */
-/*   Updated: 2025/03/20 12:14:40 by mmiguelo         ###   ########.fr       */
+/*   Updated: 2025/03/26 13:06:06 by mmiguelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+/**
+ * @brief Searches for an environment variable in the shell's list.
+ * 
+ * This function scans the shell's environment (`shell->envp`) for a variable
+ * that matches `var`. It returns the index of the variable if found, or -1
+ * if the variable is not found or invalid.
+ * 
+ * @param var  The name of the environment variable to search for.
+ * @param shell The shell structure containing the environment (`shell->envp`).
+ * @return int The index of the variable in `shell->envp` if found.
+ * 		   -1 if the variable is not found or invalid.
+ */
 int	get_env_line(char *var, t_bt *shell)
 {
 	int		i;
@@ -34,6 +46,20 @@ int	get_env_line(char *var, t_bt *shell)
 	return (-1);
 }
 
+/**
+ * @brief Checks if a command is a built-in function and returns its
+ * corresponding function pointer.
+ * 
+ * This function compares the given command (`cmd`) with a predefined
+ * list of shell built-in commands.
+ * If a match is found, it returns a pointer to the corresponding built-in function.
+ * Otherwise, it returns `NULL`, indicating that the command is not a built-in.
+ * 
+ * @param cmd The command string to check.
+ * @param shell A pointer to the shell structure (unused in this function).
+ * @return t_builtin A function pointer to the matching built-in command.
+ * 		   `NULL` if the command is not a built-in.
+ */
 t_builtin	ft_isbuiltin(char *cmd, t_bt *shell)
 {
 	static char	*builtins[8] = {
