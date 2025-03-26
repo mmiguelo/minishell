61b06e9227976db257e33b61d573aed870d1b377
mmiguelo <marco_miguelote@hotmail.com>
2025-03-26 11:33:43 +0000

    added first conditions to ft_cd

--- src/builtin/cd.c
+++ src/builtin/cd.c
@@ -6,7 +6,7 @@
 /*   By: mmiguelo <mmiguelo@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 11:19:37 by mmiguelo          #+#    #+#             */
-/*   Updated: 2025/03/20 10:41:39 by mmiguelo         ###   ########.fr       */
+/*   Updated: 2025/03/26 11:32:09 by mmiguelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,10 @@
 int	ft_cd(char **args, t_bt *shell)
 {
 	(void)shell;
-	(void)args;
+	if (!args[1])
+		printf("change directory to HOME\n");
+	else if ((args[1][0] == '-') && args[1][1])
+		printf("minishell: cd: %s: invalid option\n", args[1]);
 	return (0);
 }
+
