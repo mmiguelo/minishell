d5cfe77b64b71482e89b4b5b53e96188fb8f9c88
mmiguelo <marco_miguelote@hotmail.com>
2025-03-26 14:13:29 +0000

    clear norminette errors

--- src/free.c
+++ src/free.c
@@ -6,17 +6,20 @@
 /*   By: mmiguelo <mmiguelo@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 14:37:52 by mmiguelo          #+#    #+#             */
-/*   Updated: 2025/03/26 14:07:58 by mmiguelo         ###   ########.fr       */
+/*   Updated: 2025/03/26 14:13:08 by mmiguelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 /**
- * @brief Frees the memory allocated for the environment variables array in the shell structure.
+ * @brief Frees the memory allocated for the environment variables array in
+ * the shell structure.
  * 
- * @param shell The shell structure containing the array of environment variables (`envp`).
- * @param i The number of environment variables in the `envp` array that should be freed.
+ * @param shell The shell structure containing the array of environment
+ * variables (`envp`).
+ * @param i The number of environment variables in the `envp` array that should
+ * be freed.
  * @return int Returns 0 upon successful completion.
  */
 int	free_envp(t_bt **shell, int i)
@@ -31,7 +34,8 @@ int	free_envp(t_bt **shell, int i)
 }
 
 /**
- * @brief Frees the memory allocated for the command array in the shell structure.
+ * @brief Frees the memory allocated for the command array in the shell
+ * structure.
  * 
  * @param shell The shell structure containing the array of commands (`cmd`).
  * @param i The number of commands in the `cmd` array that should be freed.
