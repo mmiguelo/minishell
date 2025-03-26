5d310d4d590e8cbee7f3ea09e8e2d12746034999
mmiguelo <marco_miguelote@hotmail.com>
2025-03-26 14:31:33 +0000

    clear norminette errors

--- src/builtin/export2.c
+++ src/builtin/export2.c
@@ -6,9 +6,8 @@
 /*   By: mmiguelo <mmiguelo@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/21 12:14:55 by mmiguelo          #+#    #+#             */
-/*   Updated: 2025/03/25 12:42:45 by mmiguelo         ###   ########.fr       */
+/*   Updated: 2025/03/26 14:31:18 by mmiguelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
