f0033e37e52c54705c8406b9e64b616d965eaef5
mmiguelo <marco_miguelote@hotmail.com>
2025-03-26 14:06:52 +0000

    added documentation to functions

--- src/free.c
+++ src/free.c
@@ -6,7 +6,7 @@
 /*   By: mmiguelo <mmiguelo@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 14:37:52 by mmiguelo          #+#    #+#             */
-/*   Updated: 2025/03/25 12:18:10 by mmiguelo         ###   ########.fr       */
+/*   Updated: 2025/03/26 14:06:33 by mmiguelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,17 @@ int	free_envp(t_bt **shell, int i)
 	return (0);
 }
 
+/**
+ * @brief Frees the memory allocated for the command array in the shell structure.
+ * 
+ * This function iterates through the array of commands (`cmd`) in the shell structure, 
+ * freeing the memory allocated for each command string. After freeing all commands, 
+ * the array itself is freed, and the `cmd` pointer is set to `NULL` to prevent dangling pointers.
+ * 
+ * @param shell The shell structure containing the array of commands (`cmd`).
+ * @param i The number of commands in the `cmd` array that should be freed.
+ * @return int Returns 0 upon successful completion.
+ */
 int	free_cmd(t_bt **shell, int i)
 {
 	while (i > 0)
