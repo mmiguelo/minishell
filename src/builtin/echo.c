21ef3934b8eb06fa674875ebfb246c875c19f31a
mmiguelo <marco_miguelote@hotmail.com>
2025-03-26 13:46:44 +0000

    added documentation to functions

--- src/builtin/echo.c
+++ src/builtin/echo.c
@@ -6,12 +6,23 @@
 /*   By: mmiguelo <mmiguelo@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 11:19:50 by mmiguelo          #+#    #+#             */
-/*   Updated: 2025/03/20 10:41:45 by mmiguelo         ###   ########.fr       */
+/*   Updated: 2025/03/26 13:46:28 by mmiguelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+/**
+ * @brief Checks if the given argument is the string "-n".
+ * 
+ * This function checks if the provided argument `arg` starts with a minus sign
+ * (`-`) and is followed only by the letter 'n'. If the argument matches the 
+ * pattern `"-n"` (with no additional characters after the 'n'), the function 
+ * returns `1`. Otherwise, it returns `0`.
+ * 
+ * @param arg The argument string to check.
+ * @return int Returns 1 if the argument is "-n", 0 otherwise.
+ */
 static int	ft_is_n(char *arg)
 {
 	int	i;
