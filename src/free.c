ed4ec822ea3b72388824c74d331d1e871859f25b
mmiguelo <marco_miguelote@hotmail.com>
2025-03-26 14:08:17 +0000

    added documentation to functions

--- src/free.c
+++ src/free.c
@@ -6,12 +6,19 @@
 /*   By: mmiguelo <mmiguelo@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 14:37:52 by mmiguelo          #+#    #+#             */
-/*   Updated: 2025/03/26 14:06:33 by mmiguelo         ###   ########.fr       */
+/*   Updated: 2025/03/26 14:07:58 by mmiguelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+/**
+ * @brief Frees the memory allocated for the environment variables array in the shell structure.
+ * 
+ * @param shell The shell structure containing the array of environment variables (`envp`).
+ * @param i The number of environment variables in the `envp` array that should be freed.
+ * @return int Returns 0 upon successful completion.
+ */
 int	free_envp(t_bt **shell, int i)
 {
 	while (i > 0)
@@ -25,10 +32,6 @@ int	free_envp(t_bt **shell, int i)
 
 /**
  * @brief Frees the memory allocated for the command array in the shell structure.
- * 
- * This function iterates through the array of commands (`cmd`) in the shell structure, 
- * freeing the memory allocated for each command string. After freeing all commands, 
- * the array itself is freed, and the `cmd` pointer is set to `NULL` to prevent dangling pointers.
  * 
  * @param shell The shell structure containing the array of commands (`cmd`).
  * @param i The number of commands in the `cmd` array that should be freed.
