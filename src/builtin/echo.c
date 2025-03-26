94da66e7a2c11e40a1a9aa35dcd522ebc15b7c18
mmiguelo <marco_miguelote@hotmail.com>
2025-03-26 13:49:19 +0000

    added documentation to functions

--- src/builtin/echo.c
+++ src/builtin/echo.c
@@ -6,7 +6,7 @@
 /*   By: mmiguelo <mmiguelo@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 11:19:50 by mmiguelo          #+#    #+#             */
-/*   Updated: 2025/03/26 13:46:28 by mmiguelo         ###   ########.fr       */
+/*   Updated: 2025/03/26 13:48:43 by mmiguelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,22 @@ static int	ft_is_n(char *arg)
 	return (1);
 }
 
+/**
+ * @brief Prints the arguments to the standard output, with an optional newline.
+ * 
+ * This function implements the `echo` built-in command. It prints the arguments
+ * provided to the standard output (`stdout`). If the argument `-n` is used, the 
+ * function suppresses the trailing newline that is usually printed at the end.
+ * 
+ * The function checks if the `-n` flag is set (using the `ft_is_n` helper function),
+ * and if so, it avoids printing a newline at the end. Otherwise, it prints a newline 
+ * after the arguments.
+ * 
+ * @param args Array of command arguments. The first argument (`args[0]`) is usually 
+ *               `"echo"`, and subsequent arguments are the strings to be printed.
+ * @param shell Pointer to the shell structure (unused in this function).
+ * @return int Always returns `0` indicating successful execution.
+ */
 int	ft_echo(char **args, t_bt *shell)
 {
 	int	i;
