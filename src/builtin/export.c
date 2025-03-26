4888b35b97c43c4f47b6f47d7d55e0a770122438
mmiguelo <marco_miguelote@hotmail.com>
2025-03-26 14:33:09 +0000

    clear norminette errors

--- src/builtin/export.c
+++ src/builtin/export.c
@@ -6,7 +6,7 @@
 /*   By: mmiguelo <mmiguelo@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 11:20:37 by mmiguelo          #+#    #+#             */
-/*   Updated: 2025/03/26 13:58:26 by mmiguelo         ###   ########.fr       */
+/*   Updated: 2025/03/26 14:32:59 by mmiguelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,18 +32,21 @@ void	handle_export_var(char **arg, t_bt *shell, char *var, char *value)
 }
 
 /**
- * @brief Processes and validates environment variable assignments from the export command.
+ * @brief Processes and validates environment variable assignments from the
+ * export command.
  * 
- * This function processes each argument passed to the `export` command, separating
- * the variable names and their values. It validates the variables, and if valid, 
- * it calls a handler function to perform the export action. If a variable is invalid, 
- * it prints an error message. The function also prints the variable and its value for debugging purposes.
+ * This function processes each argument passed to the `export` command,
+ * separating the variable names and their values. It validates the variables,
+ * and if valid, it calls a handler function to perform the export action. If a
+ * variable is invalid, it prints an error message. The function also prints the
+ * variable and its value for debugging purposes.
  * 
- * @param args Array of strings representing the arguments passed to the `export` command. 
- *              Each argument can be a variable assignment of the form `VAR=VALUE`.
- * @param shell Pointer to the shell structure, which contains the environment variables and other data.
- * @return int Returns `0` upon successful completion. If an error occurs (such as an invalid variable),
- *              it prints an error message and returns `0`.
+ * @param args Array of strings representing the arguments passed to the `export`
+ * command. Each argument can be a variable assignment of the form `VAR=VALUE`.
+ * @param shell Pointer to the shell structure, which contains the environment
+ * variables and other data.
+ * @return int Returns `0` upon successful completion. If an error occurs (such
+ * as an invalid variable), it prints an error message and returns `0`.
  */
 int	check_export_var(char **args, t_bt *shell)
 {
